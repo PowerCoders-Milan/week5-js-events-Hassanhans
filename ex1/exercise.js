4d8b2f6f949e0d7599f8ccb4b7eee9790929820b
hassanwasswa <assanwasswa@gmail.com>
2021-03-04 13:00:40 +0100

    execrsie1 done

--- ex1/exercise.js
+++ ex1/exercise.js
@@ -1,3 +1,19 @@
 // define your functions here
 
+// call the functions and connect them to an even
+
+var link = document.querySelector('a');
+var strong = document.querySelectorAll('strong');
+link.addEventListener('mouseover', function (){
+    strong.forEach(function(strong){
+        strong.style.color = "yellow";
+    })
+})
+link.addEventListener('mouseover',function boldWords(){
+    link.style.fontWeight = "bold";
+})
 // call the functions and connect them to an event
+link.addEventListener('mouseout', function remove(){
+    link.style.fontWeight= "normal";
+})
+
